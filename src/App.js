4a22f4353b7b894fa24e14e6d648f602bb807b5a
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-23 15:46:31 -0800

    added useParams to BookDetail. Need to add link on home page so that you can click on books to get to book detail

--- src/App.js
+++ src/App.js
@@ -1,12 +1,26 @@
 import BookList from './components/book/BookList';
 import './App.css';
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { Route, Switch, BrowserRouter } from 'react-router-dom';
+import BookDetail from './components/book/BookDetail.js';
+import Book from './components/book/Book.js';
+import Author from './components/author/Author.js';
+import { useBookDetail } from './hooks/useBookDetail.js';
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
-      <BookList />
+      <BrowserRouter>
+        <Switch>
+          <Route exact path='/' component={BookList} />
+          <Route exact path='/books/:id' component={BookDetail} />
+          <Route exact path='/books' component={Book}/>
+          <Route exact path='/books/author' component={Author}/>
+        </Switch>
+      </BrowserRouter>
     </main>
   );
 }
