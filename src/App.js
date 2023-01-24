00b0a95ecd3511f9a53b92f9589d781fe5c4bca0
Ashton Ruiz <ashton.ruiz@yahoo.com>
2023-01-23 17:27:20 -0800

    added a home page and it links to the catalog. And the catalog has a link now back to home. However that link is staying on the home page now too. Going to debug that next

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import BookList from './components/book/BookList';
 import './App.css';
 import React from 'react';
-import ReactDOM from 'react-dom';
 import { Route, Switch, BrowserRouter } from 'react-router-dom';
 import BookDetail from './components/book/BookDetail.js';
-import Book from './components/book/Book.js';
-import Author from './components/author/Author.js';
+import Home from './components/home/Home.js';
+import { Link } from 'react-router-dom';
+
 
 function App() {
   // TODO: Add routes to books & views
   return (
     <main className="container">
       <h1>Library Catalog</h1>
+      <Link to='/'>Back to home</Link>
       <Switch>
+        <Route exact path='/' component={Home}/>
         <Route exact path='/books' component={BookList} />
         <Route exact path='/books/:id' component={BookDetail} />
       </Switch>
